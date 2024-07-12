5c568bccb3b6159f220538813e2861b1c7c9937b
miku8miku <52441374+miku8miku@users.noreply.github.com>
2024-07-12 16:38:55 +0800

    333

--- meitu测试.js
+++ meitu测试.js
@@ -44,7 +44,7 @@ const main = async () => {
     if (!SOURCE) throw "未知错误~";
     const { images, title } = await eval(GRAPHIC_SOURCE[SOURCE])();
     const thumb = images[random(0, images.length - 1)].replace(".webp", ".jpg");
-    const slicedImages = images.slice(0, 9);
+    const slicedImages = images.slice(0, 2);
   const imageBase64Array = await Promise.all(slicedImages.map(async (imageUrl) => {
     const response = await fetchData({ url: imageUrl, resultType: 'buffer',headers:{'Referer': 'https://mm.tvv.tw'}})
     return response
@@ -304,7 +304,7 @@ async function fetchData(o) {
           dataType = 'form', // 请求数据类型
           deviceType = 'mobile', // 设备类型
           resultType = 'data', // 返回数据类型
-          timeout = 1e4, // 超时时间
+          timeout = 2e4, // 超时时间
           useProxy = $.useProxy, // 是否使用代理
           autoCookie = false, // 是否自动携带cookie
           followRedirect = false, // 是否重定向
