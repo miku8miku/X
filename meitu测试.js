b6faa8823315444920b61410e445b70f19bb2cbe
miku8miku <52441374+miku8miku@users.noreply.github.com>
2024-07-12 16:32:09 +0800

    555

--- meitu测试.js
+++ meitu测试.js
@@ -44,7 +44,7 @@ const main = async () => {
     if (!SOURCE) throw "未知错误~";
     const { images, title } = await eval(GRAPHIC_SOURCE[SOURCE])();
     const thumb = images[random(0, images.length - 1)].replace(".webp", ".jpg");
-    const slicedImages = images.slice(0, 1);
+    const slicedImages = images.slice(0, 9);
   const imageBase64Array = await Promise.all(slicedImages.map(async (imageUrl) => {
     const response = await fetchData({ url: imageUrl, resultType: 'buffer',headers:{'Referer': 'https://mm.tvv.tw'}})
     return response
