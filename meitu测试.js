a6cdb0614f8ca79931218830fed7d5fab9bf3395
miku8miku <52441374+miku8miku@users.noreply.github.com>
2024-07-12 18:02:01 +0800

    222

--- meitu测试.js
+++ meitu测试.js
@@ -45,9 +45,9 @@ const main = async () => {
     if (!SOURCE) throw "未知错误~";
     const { images, title } = await eval(GRAPHIC_SOURCE[SOURCE])();
     const thumb = images[random(0, images.length - 1)].replace(".webp", ".jpg");
-    // const slicedImages = images.slice(0, 15);
-    $.msg('更新');
-  const imageBase64Array = await Promise.all(images.map(async (imageUrl) => {
+    const slicedImages = images.slice(0, 20);
+    $.msg('更新切片');
+  const imageBase64Array = await Promise.all(slicedImages.map(async (imageUrl) => {
     const response = await fetchData({ url: imageUrl, resultType: 'buffer',headers:{'Referer': 'https://mm.tvv.tw'}})
     return response
 }));
